396c2657d389bc77ffa94b65ce2053932d01a550
Amanda LeMoine <amanda1@mnlemoines.com>
2021-02-24 21:48:57 -0800

    sorting working on click, but not updating the component

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import { useEffect, useState } from "react";
 import EmployeeContext from "./Utils/EmployeeContext";
 
 function App() {
-  const [employees, setEmployees] = useState({});
+  const [employees, setEmployees] = useState([]);
   const [filterEmployeesBy, setFilterEmployeesBy] = useState();
-  const [filteredEmployees, setFilteredEmployees] = useState({});
-
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
+  let sortedEmployees = employees;
   // useEffect for API Call on page load
   useEffect(() => {
     API.search()
@@ -38,10 +38,39 @@ function App() {
     setFilterEmployeesBy(input);
   };
 
+  const handleSort = (e) => {
+    filteredEmployees.length > 0
+      ? (sortedEmployees = filteredEmployees)
+      : (sortedEmployees = employees);
+    switch (e.target.id) {
+      case "header-name":
+        sortedEmployees.sort((a, b) => {
+          return a.name.first.localeCompare(b.name.first);
+        });
+        break;
+      case "header-phone":
+        sortedEmployees.sort((a, b) => {
+          return a.phone.localeCompare(b.phone);
+        });
+        break;
+      case "header-email":
+        sortedEmployees.sort((a, b) => {
+          return a.email.localeCompare(b.email);
+        });
+        break;
+      default:
+        break;
+    }
+    console.log(sortedEmployees);
+    filteredEmployees.length > 0
+      ? setFilteredEmployees(sortedEmployees)
+      : setEmployees(sortedEmployees);
+  };
+
   return (
     <div className="App">
       <EmployeeContext.Provider
-        value={{ employees, filteredEmployees, handleInputChange }}
+        value={{ employees, filteredEmployees, handleInputChange, handleSort }}
       >
         <Header />
         <Navbar />
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,15 +1,24 @@
+import { useContext } from "react";
+import EmployeeContext from "../Utils/EmployeeContext";
 import TableData from "./TableData";
 
 function Table() {
+  const { handleSort } = useContext(EmployeeContext);
   return (
     <div>
       <table>
         <thead>
           <tr>
             <th>Image</th>
-            <th>Name</th>
-            <th>Phone Number</th>
-            <th>Email</th>
+            <th id="header-name" onClick={handleSort}>
+              Name
+            </th>
+            <th id="header-phone" onClick={handleSort}>
+              Phone Number
+            </th>
+            <th id="header-email" onClick={handleSort}>
+              Email
+            </th>
             <th>Date of Birth</th>
           </tr>
         </thead>
