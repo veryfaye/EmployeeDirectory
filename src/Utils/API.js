3799b634f92c4d71ffd42b13c6ebb755d41bf6eb
Amanda LeMoine <amanda1@mnlemoines.com>
2021-02-25 16:04:10 -0800

    Updates

--- src/Utils/API.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-import axios from "axios";
-
-// Export an object with a "search" method that searches the Giphy API for the passed query
-export default {
-  search: function() {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
-  }
-};
